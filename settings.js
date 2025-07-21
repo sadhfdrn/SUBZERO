7835a6582ff1df886012e8d8c6915f79be6f9b77
sadhfdrn <benneu40@gmail.com>
2025-07-21 15:03:42 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO-MD~M2szt2TnBCD6Rv6CE7C3", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
